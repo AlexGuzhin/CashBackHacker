2f4d46b22e95d841159dc587e3e6bd206fc39923
Alex Guzhin <growfood1989@yandex.ru>
2024-05-21 17:00:59 +0300

    решение задач

--- src/test/java/ru/netology/sevice/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/sevice/CashbackHackServiceTest.java
@@ -1,28 +1,28 @@
 package ru.netology.sevice;
 
-import org.testng.annotations.Test;
+
 import ru.netology.service.CashbackHackService;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 public class CashbackHackServiceTest {
 
-    @Test
+    @org.junit.Test
     public void testRemain() {
-        CashbackHackService Service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
 
-    @Test
+    @org.junit.Test
     public void testRemainWithCashback() {
-        CashbackHackService Service = new CashbackHackService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
